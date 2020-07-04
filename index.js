49188564766e23515de6e5da1384073ad8b53e7d
Gabriel <57105768+bedivere-gabriel@users.noreply.github.com>
2020-07-04 17:10:26 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function myFunction(){
+    document.getElementById("bodyID").classList.remove("disable-scroll");
+    document.getElementById("coverphoto").classList.add("active");
+    document.getElementById("activeslide").classList.add("active");
+    document.getElementById("imgslider").classList.add("active");
+    enableScroll()
+  }
