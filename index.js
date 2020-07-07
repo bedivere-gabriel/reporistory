8d6bb74091c0feba2aaeaaea9508c2db855a0ab1
Gabriel <bedivere-gabriel@github.com>
2020-07-07 23:08:31 +0800

    Patch 3.2

--- index.js
+++ index.js
@@ -6,4 +6,41 @@ function myFunction(){
     document.getElementById("imgslider").classList.add("active");
   }
 
- 
+  var myVar;
+
+  function loadfunction() {
+    myVar = setTimeout(transfunction, 2000);
+  }
+  
+  function transfunction() {
+    document.getElementById("bg_loader").classList.add("trans");
+    document.getElementById("loader").style.display = "none";
+    myVar = setTimeout(showPage,800);
+  }
+
+  function showPage() {
+    document.getElementById("bg_loader").style.display = "none";
+  }
+
+  var checkbox = document.querySelector('input[name=theme]');
+
+  checkbox.addEventListener('change', function() {
+      if(this.checked) {
+          trans()
+          document.documentElement.setAttribute('data-theme', 'light')
+          document.getElementById("navigation").classList.remove("bg-dark", "navbar-dark")
+          document.getElementById("navigation").classList.add("bg-light", "navbar-light")
+      } else {
+          trans()
+          document.documentElement.setAttribute('data-theme', 'dark')
+          document.getElementById("navigation").classList.remove("bg-light", "navbar-light")
+          document.getElementById("navigation").classList.add("bg-dark", "navbar-dark")
+      }
+  })
+  
+  let trans = () => {
+      document.documentElement.classList.add('transition');
+      window.setTimeout(() => {
+          document.documentElement.classList.remove('transition')
+      }, 1000)
+  }
